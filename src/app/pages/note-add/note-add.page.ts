89e2c14a40bb2deb7e49a8deeccdf7e8772da61c
highjump <lixiang901112@163.com>
2019-06-10 22:47:45 +0800

    Added toast when adding note

--- src/app/pages/note-add/note-add.page.ts
+++ src/app/pages/note-add/note-add.page.ts
@@ -3,7 +3,7 @@ import {NavService} from '../../services/nav.service';
 import {Client} from '../../models/client';
 import {ApiService} from '../../services/api/api.service';
 import {BasePage} from '../base.page';
-import {LoadingController, NavController} from '@ionic/angular';
+import {LoadingController, NavController, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-note-add',
@@ -17,6 +17,7 @@ export class NoteAddPage extends BasePage implements OnInit {
   constructor(
     public navCtrl: NavController,
     public loadingCtrl: LoadingController,
+    public toastController: ToastController,
     public nav: NavService,
     public api: ApiService
   ) {
@@ -47,6 +48,14 @@ export class NoteAddPage extends BasePage implements OnInit {
 
     try {
       await this.api.saveClientNote(this.client);
+
+      // show notice
+      const toast = await this.toastController.create({
+        color: 'dark',
+        message: 'Note has been saved successfully.',
+        duration: 2000
+      });
+      toast.present();
 
       // back to prev page
       this.navCtrl.pop();
